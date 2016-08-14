75adb2bdac4c30aef0d007b6b2cf02d36e8f6dd2
Andreas Abilio <andreas@abilio.dk>
2016-08-14 13:57:24 +0200

    final setup

--- webpack.config.js
+++ webpack.config.js
@@ -15,17 +15,10 @@ module.exports = {
         }
       },
       {
-        // SASS compiling && loading
         test: /\.scss$/,
         loaders: ['style', 'css', 'sass']
       },
       {
-        // CSS loading
-        test: /\.css$/,
-        loaders: ['style', 'css?sourceMap']
-      },
-      {
-        // Font loading (via css)
         test: /\.(eot|svg|ttf|woff|woff2)$/,
         loader: 'file?name=public/fonts/[name].[ext]'
       }
