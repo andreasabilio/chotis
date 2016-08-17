c140788230a4a8e1f411c02287f4d1e67984ba2b
andreasabilio <andreas@abilio.dk>
2016-08-17 20:55:34 +0200

    added images and some api

--- backend/init/api.js
+++ backend/init/api.js
@@ -3,11 +3,6 @@ var _          = require('lodash');
 var path       = require('path');
 var router     = require('koa-router');
 var importer   = require('../util/importer');
-//var requireAll = require('require-all');
-//var _endpoints  = requireAll(path.resolve(__dirname, './endpoints'));
-
-// XXX
-//console.log('+++', endpoints);
 
 //var endpoints = {
 //  image: [
@@ -22,43 +17,43 @@ var importer   = require('../util/importer');
 //  video: [],
 //  import: []
 //};
-
-
-var handlers = {
-
-  _get: function*(next){
-
-    // Has
-    if(this.params.id){
-      this.body = this.chotis.store.getItem(this.params.id);
-
-    }
-  },
-
-  //_post: function*(next){
-  //  yield next;
-  //},
-
-  // Patch item
-  _patch: function*(next){
-
-    var stored  = this.chotis.store.getItem(this.params.id);
-    var patched = Object.assign(stored, this.body);
-
-    this.chotis.store.setItem(this.params.id, patched);
-
-    yield next;
-  },
-
-  _delete: function*(next){
-    this.chotis.store.removeItem(this.params.id);
-  }
-};
-
-var types   = ['image', 'video'];
-var methods = Object.keys(handlers).map(function(_method){
-  return _method.substr('_', '');
-});
+//
+//
+// var handlers = {
+//
+//   _get: function*(next){
+//
+//     // Has
+//     if(this.params.id){
+//       this.body = this.chotis.store.getItem(this.params.id);
+//
+//     }
+//   },
+//
+//   //_post: function*(next){
+//   //  yield next;
+//   //},
+//
+//   // Patch item
+//   _patch: function*(next){
+//
+//     var stored  = this.chotis.store.getItem(this.params.id);
+//     var patched = Object.assign(stored, this.body);
+//
+//     this.chotis.store.setItem(this.params.id, patched);
+//
+//     yield next;
+//   },
+//
+//   _delete: function*(next){
+//     this.chotis.store.removeItem(this.params.id);
+//   }
+// };
+//
+// var types   = ['image', 'video'];
+// var methods = Object.keys(handlers).map(function(_method){
+//   return _method.replace('_', '');
+// });
 
 
 module.exports = function(chotis){
@@ -66,11 +61,29 @@ module.exports = function(chotis){
   // XXX
   console.log('--- running api init', chotis);
 
-  //types.forEach(function(type){
-  //  methods.forEach(function(method){
-  //    router[method]('');
-  //  });
-  //});
+
+
+  // Item crud
+  router.get('/:id', function*(next){
+    this.body = this.chotis.store.getItem(this.params.id);
+  });
+  router.patch('/:id', function*(next){
+    var stored  = this.chotis.store.getItem(this.params.id);
+    var patched = Object.assign(stored, this.body);
+    this.chotis.store.setItem(this.params.id, patched);
+  });
+  router.del('/:id', function*(next){
+    this.chotis.store.removeItem(this.params.id);
+  });
+
+
+
+  // Import
+  router.post('/import', function*(next){
+    // TODO
+    yield importer.bind(this.chotis, this.params);
+  });
+
 
 
   // Tags
