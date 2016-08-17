898fdbb4d799cf84068648edde4f8d4eba260a75
Andreas Abilio <andreas@abilio.dk>
2016-08-17 20:22:44 +0200

    partial tags endpoints

--- backend/init/api.js
+++ backend/init/api.js
@@ -7,43 +7,88 @@ var importer   = require('../util/importer');
 //var _endpoints  = requireAll(path.resolve(__dirname, './endpoints'));
 
 // XXX
-console.log('+++', endpoints);
+//console.log('+++', endpoints);
 
-var endpoints = {
-  image: [
-    {
-      pattern: '',
-      method: '',
-      handler: function*(next){
+//var endpoints = {
+//  image: [
+//    {
+//      pattern: '',
+//      method: '',
+//      handler: function*(next){
+//
+//      }
+//    }
+//  ],
+//  video: [],
+//  import: []
+//};
 
-      }
+
+var handlers = {
+
+  _get: function*(next){
+
+    // Has
+    if(this.params.id){
+      this.body = this.chotis.store.getItem(this.params.id);
+
     }
-  ],
-  video: [],
-  import: []
+  },
+
+  //_post: function*(next){
+  //  yield next;
+  //},
+
+  // Patch item
+  _patch: function*(next){
+
+    var stored  = this.chotis.store.getItem(this.params.id);
+    var patched = Object.assign(stored, this.body);
+
+    this.chotis.store.setItem(this.params.id, patched);
+
+    yield next;
+  },
+
+  _delete: function*(next){
+    this.chotis.store.removeItem(this.params.id);
+  }
 };
+
+var types   = ['image', 'video'];
+var methods = Object.keys(handlers).map(function(_method){
+  return _method.substr('_', '');
+});
 
 
 module.exports = function(chotis){
 
-  var handlers = {
-    fetch: function*(next){
-
-      var params = this.params;
-      var type   = params.type || null;
-      var id     = params.id || null;
-
-
-
-
-    }
-  };
-
   // XXX
   console.log('--- running api init', chotis);
 
-  router.post('/import', function*(next){
+  //types.forEach(function(type){
+  //  methods.forEach(function(method){
+  //    router[method]('');
+  //  });
+  //});
 
+
+  // Tags
+  router.get('/tag', function*(next){
+    this.body = this.chotis.store.getItem('__tags');
+  });
+  router.get('/tag/:id', function*(next){
+    this.body = this.chotis.store.values().filter(item => {
+      return -1 !== item.tags.indexOf(this.params.id);
+    });
+  });
+  router.post('/tag', function*(next){
+    var tag  = this.body;
+    var tags = this.chotis.store.getItem('__tags') || [];
+    tags.push(tag);
+  });
+  router.del('/tag/:id', function*(next){
+    this.chotis.store.removeItem(this.params.id);
   });
 
 
